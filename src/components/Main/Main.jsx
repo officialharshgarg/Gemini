d45f7865993bf323d9d874219703a2411910dbd7
HARSH GARG <officialharshgarg>
2024-08-04 02:30:33 +0530

    Add error boundary and update project files

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
+import React, { useContext } from "react";
 import "./Main.css";
-import { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 
@@ -32,40 +32,22 @@ const Main = () => {
               <p>How can I help you today?</p>
             </div>
             <div className="cards">
-              <div
-                onClick={() => onSent("Suggest some places for the trip")}
-                className="card"
-              >
+              <div onClick={() => onSent("Suggest some places for the trip")} className="card">
                 <p>Suggest some places for the trip</p>
                 <img src={assets.compass_icon} alt="" />
               </div>
 
-              <div
-                onClick={() => onSent("Briefly summarize: Urban Planning")}
-                className="card"
-              >
+              <div onClick={() => onSent("Briefly summarize: Urban Planning")} className="card">
                 <p>Briefly summarize: Urban Planning</p>
                 <img src={assets.bulb_icon} alt="" />
               </div>
 
-              <div
-                onClick={() =>
-                  onSent(
-                    "Brainstorm team bonding activities for our work retreat"
-                  )
-                }
-                className="card"
-              >
+              <div onClick={() => onSent("Brainstorm team bonding activities for our work retreat")} className="card">
                 <p>Brainstorm team bonding activities for our work retreat</p>
                 <img src={assets.message_icon} alt="" />
               </div>
 
-              <div
-                onClick={() =>
-                  onSent("Tell me about React js and React native")
-                }
-                className="card"
-              >
+              <div onClick={() => onSent("Tell me about React js and React native")} className="card">
                 <p>Tell me about React js and React native</p>
                 <img src={assets.code_icon} alt="" />
               </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
